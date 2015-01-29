188e12994d1d8ad469c4251aa2dfae9859bb2f0b
Steven Vergenz <steve.vergenz.ctr@adlnet.gov>
2015-01-29 16:09:26 -0500

    Initial commit of experimental code

---
+++ xAPIAgentComponent.cpp
@@ -0,0 +1,13 @@
+
+
+#include "xAPIIntegration.h"
+#include "xAPIAgentComponent.h"
+
+
+UxAPIAgentComponent::UxAPIAgentComponent(const class FPostConstructInitializeProperties& PCIP)
+	: Super(PCIP)
+{
+
+}
+
+
---
+++ xAPIAgentComponent.h
@@ -0,0 +1,76 @@
+
+
+#pragma once
+
+#include "Components/ActorComponent.h"
+#include "xAPIAgentComponent.generated.h"
+
+
+/*UENUM(BlueprintType)
+enum class EAgentIFIType : uint8
+{
+	MBOX			UMETA(DisplayName = "mbox"),
+	MBOX_SHA1SUM	UMETA(DisplayName = "mbox_sha1sum"),
+	OPENID			UMETA(DisplayName = "openid"),
+	ACCOUNT			UMETA(DisplayName = "account")
+};*/
+
+UENUM(BlueprintType)
+namespace EAgentIFIType
+{
+	enum Type {
+		MBOX UMETA(DisplayName = "mbox"),
+		MBOX_SHA1SUM UMETA(DisplayName = "mbox_sha1sum"),
+		OPENID UMETA(DisplayName = "openid"),
+		ACCOUNT UMETA(DisplayName = "account")
+	};
+}
+
+USTRUCT()
+struct FAgentAccountInfo
+{
+	GENERATED_USTRUCT_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = ExperienceAPI)
+		FString Homepage;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = ExperienceAPI)
+		FString Name;
+
+	FAgentAccountInfo(){
+		Homepage = TEXT("");
+		Name = TEXT("");
+	}
+};
+
+/**
+ * A component representing an xAPI Agent, i.e. a learner
+ */
+UCLASS(BlueprintType)
+class XAPIINTEGRATION_API UxAPIAgentComponent : public UActorComponent
+{
+	GENERATED_UCLASS_BODY()
+
+public:
+
+	UPROPERTY(EditAnywhere, Category = ExperienceAPI)
+		FString Name;
+
+	UPROPERTY(EditAnywhere, Category = ExperienceAPI)
+		//EAgentIFIType ifi_type;
+		TEnumAsByte<EAgentIFIType::Type> ifi_type;
+
+	UPROPERTY(EditAnywhere, Category = ExperienceAPI)
+		FString Mbox;
+
+	UPROPERTY(EditAnywhere, Category = ExperienceAPI)
+		FString MboxSha1Sum;
+
+	UPROPERTY(EditAnywhere, Category = ExperienceAPI)
+		FString OpenID;
+
+	UPROPERTY(EditAnywhere, Category = ExperienceAPI)
+		FAgentAccountInfo Account;
+
+	//UxAPIAgentComponent(const class FPostConstructInitializeProperties& PCIP);
+};
